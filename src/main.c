b3589071b709613e1f6415b15941d89bd653cdcc
Johannes Pfeiffer <johannespfeiffer@pop-os.localdomain>
2021-02-25 22:35:56 +0100

    simple SDL2 setup

--- src/main.c
+++ src/main.c
@@ -4,6 +4,9 @@
 
 void main()
 {
-	printf("hallo");
+	/*Initialization*/
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window *window=SDL_CreateWindow("Simple Screen Test Tool", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+	SDL_Delay(3000);
 }
 
