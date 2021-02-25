695bf991d42cf24b368cb78e0781c3a3a5635267
tevoran <johannespfeiffer@pop-os.localdomain>
2021-02-26 00:03:14 +0100

    simple SDL2 setup

--- src/main.c
+++ src/main.c
@@ -7,6 +7,6 @@ void main()
 	/*Initialization*/
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window *window=SDL_CreateWindow("Simple Screen Test Tool", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
-	SDL_Delay(3000);
+	SDL_Delay(3500);
 }
 
